4aa31faa61b7abfafa5e2c63156a2c3b03924c36
Eric <ericjang310@gmail.com>
2020-11-17 14:10:20 -0800

    new team dialog

--- client/src/pages/Teams.js
+++ client/src/pages/Teams.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect }  from 'react';
 
+import Button from "@material-ui/core/Button";
+
 import ValTable from '../components/ValTable';
+import NewTeamDialog from '../components/NewTeamDialog';
 
 const Teams = () => {
+    const [dialogOpen, setDialogOpen] = useState(false);
     const [state, setState] = useState({ results: [], columns: [] });
+
+    const handleClose = () => {
+        setDialogOpen(false);
+    }
+
+    const handleNewTeamClick = () => {
+        setDialogOpen(true);
+    }
 
     useEffect(() => {
         fetch('/sql', {
@@ -16,7 +28,14 @@ const Teams = () => {
             .then(teams => setState({ results: teams['results'], columns: teams['columns'] }));
     }, [])
 
-    return <ValTable tableName="Teams" results={state.results} columns={state.columns}></ValTable>;
+    return (
+        <>
+            <ValTable tableName="Teams" results={state.results} columns={state.columns}></ValTable>
+            <br/>
+            <Button variant="contained" onClick={handleNewTeamClick}>CREATE NEW TEAM</Button>
+            <NewTeamDialog open={dialogOpen} handleClose={handleClose} />
+        </>
+    );
 }
 
 export default Teams;
