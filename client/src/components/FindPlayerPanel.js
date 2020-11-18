7bd3ea51bb8c2b2417afdf031f6c4291b261a9d8
unknown <vandyliu1999@gmail.com>
2020-11-18 11:22:15 -0800

    update styles

--- client/src/components/FindPlayerPanel.js
+++ client/src/components/FindPlayerPanel.js
@@ -12,7 +12,8 @@ import Typography from '@material-ui/core/Typography';
 const FindPlayerPanel = ({ values, handleSubmit }) => {
     const useStyles = makeStyles({
         root: {
-            padding: 4,
+            padding: '1rem',
+            margin: '0rem 0rem 3rem 0rem',
             display: "flex",
             flexDirection: "row"
         },
@@ -24,7 +25,12 @@ const FindPlayerPanel = ({ values, handleSubmit }) => {
             width: 200,
             marginRight: 8
         },
+        title: {
+            "font-family": 'valorant',
+            "text-align": "center"
+        },
     });
+    
     const classes = useStyles();
 
     const onFormChange = (field, value) => {
@@ -33,7 +39,7 @@ const FindPlayerPanel = ({ values, handleSubmit }) => {
 
     return (
         <>
-        <Typography variant="h6">Find Players</Typography>
+        <Typography className={classes.title} variant="h5">Find Players</Typography>
         <Paper className={classes.root}>
             <FormControl className={classes.form} variant="outlined">
                 <InputLabel>Rank</InputLabel>
--- client/src/pages/Agents.js
+++ client/src/pages/Agents.js
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
 
 import ValTable from '../components/ValTable';
 
 const Agents = () => {
     const [data, setData] = useState({ results: [], columns: [] });
+
+    const useStyles = makeStyles({
+        table: {
+            minWidth: 650
+        },
+        title: {
+            "font-family": 'valorant',
+            "text-align": "center"
+        },
+        container: {
+            "padding": '2rem'
+        }
+    });
+
+    const classes = useStyles();
 
     useEffect(() => {
         fetch('/sql', {
@@ -21,7 +38,11 @@ const Agents = () => {
             });
     }, [])
 
-    return <ValTable tableName="Agents" results={data.results} columns={data.columns}></ValTable>;
+
+    return (
+        <Container className={classes.container} maxWidth="lg">
+            <ValTable tableName="Agents" results={data.results} columns={data.columns}></ValTable>;
+        </Container>);
 }
 
 export default Agents;
