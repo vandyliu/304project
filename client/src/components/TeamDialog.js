663d9e945501a147cb7091d7f714843fca5a6075
Eric <ericjang310@gmail.com>
2020-11-17 22:03:43 -0800

    add edit button for team

---
+++ client/src/components/TeamDialog.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+
+const TeamDialog = ({ open, title, initState, handleClose, handleSubmit, submitButtonText }) => {
+    const [form, setForm] = useState(initState ?? { id: 0, name: "", wins: 0, losses: 0 });
+
+    useEffect(() => {
+        if (initState) setForm(initState);
+    }, [initState])
+
+    const handleFormField = (field, value) => {
+        setForm({ ...form, [field]: value })
+    }
+
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+        >
+            <DialogTitle id="form-dialog-title">{title}</DialogTitle>
+            <DialogContent>
+            <DialogContentText>
+                Enter the team details below.
+            </DialogContentText>
+            <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Team ID"
+                type="number"
+                value={form.id}
+                onChange={(e) => handleFormField("id", e.target.value)}
+                fullWidth
+            />
+            <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Name"
+                type="name"
+                value={form.name}
+                onChange={(e) => handleFormField("name", e.target.value)}
+                fullWidth
+            />
+            <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Wins"
+                type="number"
+                value={form.wins}
+                onChange={(e) => handleFormField("wins", e.target.value)}
+                fullWidth
+            />
+            <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Losses"
+                type="number"
+                value={form.losses}
+                onChange={(e) => handleFormField("losses", e.target.value)}
+                fullWidth
+            />
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleClose} color="primary">
+                Cancel
+            </Button>
+            <Button onClick={() => handleSubmit(form)} color="primary">
+                {submitButtonText}
+            </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default TeamDialog;
--- client/src/components/TeamsAccordion.js
+++ client/src/components/TeamsAccordion.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState }  from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from '@material-ui/core/Typography';
 
-const TeamsAccordion = ({ title, teams, teamPlayers }) => {
+import EditTeamDialog from './EditTeamDialog';
+
+const TeamsAccordion = ({ title, teams, teamPlayers, onEditCallback }) => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedTeam, setSelectedTeam] = useState(null);
     const useStyles = makeStyles({
         table: {
             minWidth: 650
@@ -22,10 +27,24 @@ const TeamsAccordion = ({ title, teams, teamPlayers }) => {
         list: {
             display: "flex",
             flexDirection: "column",
+            alignItems: "center"
+        },
+        button: {
+            marginBottom: "4px",
+            width: '50%',
+            textAlign: 'center'
         }
     });
-
     const classes = useStyles();
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    }
+
+    const handleOpenModal = (team) => {
+        setSelectedTeam(team);
+        setModalOpen(true);
+    }
 
     return (
         <div className={classes.table}>
@@ -43,6 +62,9 @@ const TeamsAccordion = ({ title, teams, teamPlayers }) => {
                     <Typography className={classes.secondaryHeading}>Wins: {team.wins} Losses: {team.losses}</Typography>
                 </AccordionSummary>
                 <AccordionDetails className={classes.list}>
+                    <Button onClick={() => handleOpenModal(team)} variant="contained" className={classes.button}>
+                        Edit Team
+                    </Button>
                     {teamPlayers[team.team_id]?.map((player) => (
                         <Typography key={player.player_id}>
                             {player.player_id}
@@ -51,6 +73,12 @@ const TeamsAccordion = ({ title, teams, teamPlayers }) => {
                 </AccordionDetails>
             </Accordion>
         ))}
+        <EditTeamDialog
+            open={modalOpen}
+            team={selectedTeam} 
+            handleClose={handleCloseModal}
+            onSubmitCallback={onEditCallback}
+        />
     </div>
 
     );
