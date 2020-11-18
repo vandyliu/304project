79a184b177364eb9b17d277e3d11927368a616d3
unknown <vandyliu1999@gmail.com>
2020-11-18 14:36:12 -0800

    add sql inserts

--- client/src/pages/Agents.js
+++ client/src/pages/Agents.js
@@ -42,7 +42,7 @@ const Agents = () => {
     return (
         <Container className={classes.container} maxWidth="lg">
             <ValTable tableName="Agents" results={data.results} columns={data.columns}></ValTable>
-        </Container>)
+        </Container>);
 }
 
 export default Agents;
